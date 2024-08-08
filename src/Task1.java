ff173c0665e00ec66ebc23a7beadf7ee87da75fe
tatyana86 <krivonogova_1999@mail.ru>
2024-08-08 21:30:16 +0400

    add solution

--- src/Task1.java
+++ src/Task1.java
@@ -1,14 +1,24 @@
 class Building {
 
 	int numberOfFloors; // количество этажей
-	double height; // высота здания
+	double height; // высота строения
+	double area; // площадь строения
 	
 	// конструктор
-	Building(int numberOfFloors, double height) {
+	Building(int numberOfFloors, double height, double area) {
 		this.numberOfFloors = numberOfFloors;
 		this.height = height;
+		this.area = area;
 	}
 
+	// возвращает прикидочный объем строения
+	double getVolume() {
+		double areaOfOneFloor = area / numberOfFloors;
+		double volume = areaOfOneFloor * height;
+		return volume;
+	}
+	
+	// возвращает высоту одного этажа
 	double getFloorHeight() {
 		return height / numberOfFloors;
 	}
@@ -18,9 +28,25 @@ class Cottage extends Building {
 	
 	double foundationHeight; // высота фундамента над землей
 	
-	Cottage(int numberOfFloors, double height, double foundationHeight) {
-		super(numberOfFloors, height); // вызов конструктора суперкласса
+	Cottage(int numberOfFloors, double height, double area, double foundationHeight) {
+		super(numberOfFloors, height, area); // вызов конструктора суперкласса
 		this.foundationHeight = foundationHeight;
+	}
+	
+	double getFloorHeight() {
+		return height / numberOfFloors + foundationHeight;
+	}
+
+}
+
+class Greenhouse extends Building {
+	
+	Window window; // окно в теплице
+	
+	// конструктор
+	Greenhouse(int numberOfFloors, double height, double area, Window window) {
+		super(numberOfFloors, height, area);
+		this.window = window;
 	}
 
 }
@@ -42,11 +68,16 @@ public class Task1 {
 
 	public static void main(String[] args) {
 		
+		// Использование композиции - объект класса "Окно" содержится как поле объекта класса "Теплица"
+		Window window = new Window(1.5, 1.0);
+		Greenhouse greenhouse = new Greenhouse(1, 1.8, 4, window);
+		
 		// Использование наследования - вызов метода, определенного в родительском классе
-		Cottage cottage = new Cottage(1, 3.0, 1.0);
-		
-		System.out.print(cottage.getFloorHeight());
-		
+		Cottage cottage = new Cottage(1, 3.0, 100, 1.0);
+		System.out.println(cottage.getVolume());
+				
+		// Использование полиморизма
+		System.out.println(cottage.getFloorHeight());
 	}
 
 }
